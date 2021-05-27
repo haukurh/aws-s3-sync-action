5473b9591a5f2c06616a41fd79b0dcb33d35bc72
Haukur Hlöðversson <haukur@haukurh.is>
2021-05-27 00:10:30 +0000

    change to js action

---
+++ main.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+let stdout = '';
+let stderr = '';
+
+const options = {};
+options.listeners = {
+    stdout: (data) => {
+        stdout += data.toString();
+    },
+    stderr: (data) => {
+        stderr += data.toString();
+    }
+};
+
+//SYNC_OUTPUT=$(aws s3 sync ${{ inputs.directory }} s3://${{ inputs.s3-bucket }}${{ inputs.path }} --no-progress ${{ inputs.args }}) && echo "::set-output name=stdout::$SYNC_OUTPUT"
+
+const directory = core.getInput('directory');
+const s3Bucket = core.getInput('s3-bucket');
+const path = core.getInput('path');
+const optionalArgs = core.getInput('args');
+
+const args = [
+    's3',
+    'sync',
+    directory,
+    `s3://${s3Bucket}${path}`,
+    '--no-progress',
+    optionalArgs,
+]
+
+await exec.exec('aws', args, options);
+
+if (stderr) {
+    core.setFailed(stderr);
+} else {
+    core.setOutput('stdout', stdout);
+}
