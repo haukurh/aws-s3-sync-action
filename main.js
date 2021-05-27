8ded713b219fb70629d8d43da2efd0908f37c3a6
Haukur Hlöðversson <haukur@haukurh.is>
2021-05-27 17:54:34 +0000

    Renamed input + refactor

--- main.js
+++ main.js
@@ -13,13 +13,18 @@ options.listeners = {
         stderr += data.toString();
     }
 };
+options.cwd = '.';
 
-//SYNC_OUTPUT=$(aws s3 sync ${{ inputs.directory }} s3://${{ inputs.s3-bucket }}${{ inputs.path }} --no-progress ${{ inputs.args }}) && echo "::set-output name=stdout::$SYNC_OUTPUT"
 
 const directory = core.getInput('directory');
 const s3Bucket = core.getInput('s3-bucket');
-const path = core.getInput('path');
+let path = core.getInput('s3-path');
 const optionalArgs = core.getInput('args');
+
+// Add slash to start of path if it's not given
+if (path[0] !== '/') {
+    path = '/' + path;
+}
 
 const args = [
     's3',
@@ -40,6 +45,5 @@ exec.exec('aws', args, options).then((r) => {
         core.setOutput('stdout', stdout);
     }
 }).catch((e) => {
-    console.error(stderr);
-    core.setFailed('AWS command failed');
+    core.setFailed(stderr);
 });
